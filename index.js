16af77c83431ee06cfef9e61549c1bb160cf3a5c
Ankan-Sadhukhan <sadhukhanankan7@gmail.com>
2024-04-17 14:42:15 +0530

    Interservice communication logic and api gateway logic added

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const  morgan = require('morgan');
 const { rateLimit } = require('express-rate-limit');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
 
 const app = express();
 
-const PORT =3005;
+const PORT =3006;
 
 app.use(morgan('combined'))
 
@@ -15,6 +16,29 @@ const limiter = rateLimit({
 })
 
 app.use(limiter)
+
+app.use('/bookingservice', async(req,res,next) => {
+    try {
+        console.log(req.headers['x-access-token']);
+        const response = await axios.get('http://localhost:3001/api/v1/isauthenticated', {
+            headers: {
+                'x-access-token': req.headers['x-access-token']
+            }
+        });
+        console.log(response.data);
+        if(response.data.success) {
+            next();
+        } else {
+            return res.status(401).json({
+                message: 'Unauthorised'
+            })
+        }
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Unauthorised'
+        })
+    }
+})
 
 app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:3002/', changeOrigin: true}));
 
